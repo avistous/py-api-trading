9f92facdd9193143750baa8323bae460929d5ba3
atyang <ayang@oanda.com>
2013-06-13 11:29:18 -0300

    Update api-order.py

--- api-order.py
+++ api-order.py
@@ -8,12 +8,12 @@ import time
 ## This demonstrates getting the current price of an instrument and trading if it is above a threshold
 def checkAndTrade():
     conn = httplib.HTTPSConnection("api-sandbox.oanda.com")
-    conn.request("GET", "/v1/instruments/USD_CAD/price")
+    conn.request("GET", "/v1/quote?instruments=USD_CAD")
     response = conn.getresponse()
     if response.status == 200:
         data = json.loads(response.read())
         if data['ask'] > 0.994:
-            conn.request("POST", "/v1/accounts/3/trades?instrument=USD/CAD&units=50&direction=long")
+            conn.request("POST", "/v1/accounts/3/trades?instrument=USD_CAD&units=50&direction=long")
             print conn.getresponse().read()
 
 
@@ -21,7 +21,7 @@ def checkAndTrade():
 def order():
     now = int(time.time())    
     conn = httplib.HTTPSConnection("api-sandbox.oanda.com")
-    params = urllib.urlencode({"instrument": "USD/CAD",
+    params = urllib.urlencode({"instrument": "USD_CAD",
                                "units" : 50,
                                "price" : 0.994,
                                "expiry" : now + 604800,
